be15cd2fc1b7aa9995f703a066fcb8f3363aa4f5
Ainsley Marsh <amarsh37@my.bcit.ca>
2021-04-27 19:14:04 -0400

    Hamburger menu added

---
+++ movie/src/routers/AppRouter.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function AppRouter() {
+    return (
+    <Router>
+        <div className="wrapper">
+          <Header />
+          
+      </div>
+    </Router>
+   
+    );
+}
+
+export default AppRouter;
