90fb764362ad54733ea85afb02ecf58dfe951769
Ainsley Marsh <amarsh37@my.bcit.ca>
2021-04-28 19:30:54 -0400

    navigation menu complete

--- movie/src/routers/AppRouter.js
+++ movie/src/routers/AppRouter.js
@@ -1,15 +1,23 @@
-import Header from '../components/Header';
-import {BrowserRouter as Router} from 'react-router-dom';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import PageHome from '../pages/PageHome';
+import PageAbout from '../pages/PageAbout';
+import PageFav from '../pages/PageFav';
+import PageAccount from '../pages/PageAccount';
+import PageLogin from '../pages/PageLogin';
 
 function AppRouter() {
     return (
     <Router>
-        <div className="wrapper">
-      <PageHome>  
-      <Header> 
-      </Header> 
-      </PageHome>
+        <div className="wrapper"> 
+      <Switch>
+      <Route path ="/" exact><PageHome/></Route>
+      <Route path="/about"><PageAbout/></Route>
+      <Route path="/favourites"><PageFav/></Route>
+      <Route path="/my-account"><PageAccount/></Route>
+      <Route path="/sign-in"><PageLogin/></Route>
+                  
+      </Switch>
       </div>
     </Router>
     )  
