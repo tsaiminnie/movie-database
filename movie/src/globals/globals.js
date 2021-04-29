daacb70acbdf891ea8220f4851542bbe0dbc91b1
Hunter Paulson <hpaulson@my.bcit.ca>
2021-04-29 14:01:44 -0700

    connected to tmdb

--- movie/src/globals/globals.js
+++ movie/src/globals/globals.js
@@ -1,1 +1,3 @@
 export const pageTitle = 'DiscoveryFlix';
+export const API_KEY = '8001a7eab9a9a130964dd2225dcb1159';
+export const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDAxYTdlYWI5YTlhMTMwOTY0ZGQyMjI1ZGNiMTE1OSIsInN1YiI6IjYwOGFlMGM5NjgwYmU4MDA1N2U1YzI3ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CXGgG7xfjfaLVO7_FdQBvRRUjyQ9i2XeHkSBXm2klws';
