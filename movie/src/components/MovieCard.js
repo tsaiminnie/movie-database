c004ab6ee074d5739055618ce49fefa5eb043f33
Minnie Tsai <ctsai73@my.bcit.ca>
2021-04-30 14:40:51 -0700

    added more hover styles and started on sortbar

--- movie/src/components/MovieCard.js
+++ movie/src/components/MovieCard.js
@@ -1,38 +1,52 @@
 import { Link } from 'react-router-dom';
-// import emptyHeart from '../images/emptyheart48.png';
-import filledHeart from '../images/filledheart48.png';
-import { useState } from 'react';
 import FavBtn from './FavBtn';
 
 function MovieCard({movie, isFav}) {
 
     function overview(){
        let str = movie.overview;
-       if (movie.overview.length > 50){
+       if (str.length > 50){
        str = str.substring(0, 50)+ "...";
        return str;
         }   
     }
 
-    // const {heart, setHeart} = useState(null);
+    function rating(){
+        let rating = movie.vote_average;
+        if (rating !== null){
+            rating = (rating * 10)+ "%";
+        }
+        return rating;
+    }
+
+    function title(){
+        let str = movie.title;
+        if (str.includes("-")){
+        str = str.substring(0, str.indexOf("-"));
+         }else{
+             str = movie.title;
+         }  
+         return str;  
+    }
+
 
     return (
         <div className="movie">
             <div className="poster">
-
-            {/* {isFav &&   <div className="heart">
-                            <img src={filledHeart}/>
-                        </div> } */}
             
                 <div className="hover-info">
 
-                <div>
+                <div className='heartandrating'>
                     {isFav ? 
                         <FavBtn movie={movie} remove={true} /> : 
                         <FavBtn movie={movie}  />}
+
+                    <p>{rating()}</p>
                 </div>
+
+                
           
-                <h3>{movie.title}</h3>
+                <h3>{title()}</h3>
                 <p>{movie.release_date}</p>
                 <p>{overview()}</p>
                 <Link to='/'>More Info</Link>
