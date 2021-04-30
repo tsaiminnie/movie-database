954a448aee3616536469789c54da159bbf2e7991
Ainsley Marsh <amarsh37@my.bcit.ca>
2021-04-30 14:44:39 -0400

    Sign in page styling

--- movie/src/pages/PageLogin.js
+++ movie/src/pages/PageLogin.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { pageTitle } from '../globals/globals';
+import Login from '../components/Login';
 import ResponsiveBg from '../components/ResponsiveBg';
 import Header from '../components/Header';
 
@@ -11,7 +12,7 @@ const PageLogin = () => {
 
     return (
         <section>
-            
+            <Login/>
         </section>
     );
 };
