a7d53a4dcd76eb41c963518c5e822a03ab5a8701
Hunter Paulson <hpaulson@my.bcit.ca>
2021-04-30 11:09:51 -0700

    yay the favourite functionality is working!

--- movie/src/components/FavBtn.js
+++ movie/src/components/FavBtn.js
@@ -1,19 +1,32 @@
+import useGlobal from '../globals/globalState';
+import emptyHeart from '../images/emptyheart48.png';
+import filledHeart from '../images/filledheart48.png';
+
 function FavBtn({ movie, remove, handleFavClick }) {
 
+    const [globalState, globalActions] = useGlobal();
+
     function addFav(){
-        handleFavClick(true, movie);
+        globalActions.addFav(movie);
     }
 
     function removeFav(){
-        handleFavClick(false, movie);
+        globalActions.removeFav(movie.id);
     }
+
+    //const {heart, setHeart} = useState(null);
+    //<img className='heart' src = { setHeart === null ? emptyHeart : filledHeart } alt = 'Heart'/>
 
     return (
         //We will add the heart onclick stuff, instead of these buttons. When Its not in favs its a heart outline, when it is then the heart is filled :)
         <>
-            {remove === false ? 
+            {/* {remove === false ? 
             <button onClick={addFav}>Add To Favs</button> : 
-            <button onClick={removeFav}>Remove From Favs</button>}
+            <button onClick={removeFav}>Remove From Favs</button>} */}
+
+            {remove === false ? 
+            <img className="heart" src={emptyHeart} onClick={addFav} /> :
+            <img className="heart" src={filledHeart} onClick={removeFav} />}
         </>
     );
 }
